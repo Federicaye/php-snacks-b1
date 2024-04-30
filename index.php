af4e37990449b0c6378124c62a1ba4e2665d58c9
Federica Gallo <gallofederica58@gmail.com>
2024-04-30 17:58:19 +0200

    snack 3

--- index.php
+++ index.php
@@ -25,21 +25,61 @@ $matches = [
         "scoreGuest" => 0,
     ],
 ];
+$posts = [
 
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Valentina Pesca',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Paolo Ricci',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Sara Ferdinando',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Francesca Grigio',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Federica Gallo',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
 
-if(isset($_GET['name'])){
+if (isset($_GET['name'])) {
     $name = $_GET['name'];
 }
-if(isset($_GET['age'])){
+if (isset($_GET['age'])) {
     $age = $_GET['age'];
 }
-if(isset($_GET['email'])){
-    $email = $_GET['email']; 
+if (isset($_GET['email'])) {
+    $email = $_GET['email'];
 }
-if(strlen($name)>3 && strpos($email, '@') && strpos($email, '.') && is_numeric($age)) {
+if (strlen($name) > 3 && strpos($email, '@') && strpos($email, '.') && is_numeric($age)) {
     $message = 'Accesso riuscito';
-} else { $message = 'accesso negato';}
-    ?>
+} else {
+    $message = 'accesso negato';
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -52,20 +92,37 @@ if(strlen($name)>3 && strpos($email, '@') && strpos($email, '.') && is_numeric($
 
 <body>
     <ul>
-        <?php foreach ($matches as $match => $value) { 
-             echo "<li>" . $matches[$match]['homeTeam'] . ' - ' . $matches[$match]['guestTeam'] . ' | ' . $matches[$match]['scoreHome'] . '-' . $matches[$match]['scoreGuest'] . "</li>" ;}
-        
+        <?php foreach ($matches as $match) {
+            echo "<li>" . $match['homeTeam'] . ' - ' . $match['guestTeam'] . ' | ' . $match['scoreHome'] . ' - ' . $match['scoreGuest'] . "</li>";
+        }
+
         ?>
-        </ul>
+    </ul>
 
-        <form action="index.php" method="GET">
-            <input type="text" name="name" placeholder="name">
-            <input type="text" name="age" placeholder="age">
-            <input type="text" name="email" placeholder="email">
-            <button type="submit">send</button>
-        </form>
+    <form action="index.php" method="GET">
+        <input type="text" name="name" placeholder="name">
+        <input type="text" name="age" placeholder="age">
+        <input type="text" name="email" placeholder="email">
+        <button type="submit">send</button>
+    </form>
 
-        <div> <?php if($name && $age && $email) { echo  $message ;} else { echo 'compila il form';}?> </div>
+    <div> <?php if ($name && $age && $email) {
+        echo $message;
+    } else {
+        echo 'compila il form';
+    } ?> </div>
+
+    <ul>
+        <?php foreach ($posts as $date => $postList) {
+            echo $date;
+            foreach ($postList as $post) {
+                echo "<li>" . ($post['text']) . ' ' . ($post['author']) . "</li>";
+            }
+
+
+        }
+        ?>
+    </ul>
 
 </body>
 
